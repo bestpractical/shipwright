9c23c745f8afb48f749e9b2535bd49dc53f53a50
sunnavy <sunnavy@bestpractical.com>
2008-03-19 08:03:02 +0000

    added Manual.pm, move Totorial.pod to Manual/

---
+++ lib/Shipwright/Manual.pm
@@ -0,0 +1,20 @@
+=head1 NAME
+
+Shipwright::Manual - Shipwright documentation
+
+=head1 DESCRIPTION
+
+The manual is divided into several parts, each covering a specific topic.
+
+=over 4
+
+=item L<Shipwright::Tutorial>
+
+A simple tutorial to help you know what shipwright generally is.
+
+=back
+
+=cut
+
+1;
+
