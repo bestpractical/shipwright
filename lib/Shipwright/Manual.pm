d13f1f761b3b2846643f1de48db92f6d0313ecc7
Christine Spang <spang@bestpractical.com>
2008-06-02 22:31:15 +0000

    More documentation updates, to the Tutorial (explain how to use built vessels) and update the Shipwright::Manual front page.

--- lib/Shipwright/Manual.pm
+++ lib/Shipwright/Manual.pm
@@ -10,7 +10,16 @@ The manual is divided into several parts, each covering a specific topic.
 
 =over 4
 
-=item L<Shipwright::Tutorial>
+=item L<Shipwright::Manual::Tutorial>
+
+A simple tutorial that leads through the creation of a simple Shipwright
+vessel.
+
+=item L<Shipwright::Manual::CustomizeBuild>
+
+A simple tutorial to help you know what shipwright generally is.
+
+=item L<Shipwright::Manual::UsingFlags>
 
 A simple tutorial to help you know what shipwright generally is.
 
@@ -23,4 +32,3 @@ Glossary for Shipwright
 =cut
 
 1;
-
