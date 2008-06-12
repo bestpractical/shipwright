92e24ed1168caad89d07082b4d738c000af3584f
sunnavy <sunnavy@bestpractical.com>
2008-06-12 14:16:24 +0000

    refactor Logger.pm, init should not rely on Shipwright object, it can be fed any stuff that has log_level and log_file info

--- lib/Shipwright/Logger.pm
+++ lib/Shipwright/Logger.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Carp;
 use Log::Log4perl;
+use Scalar::Util qw/blessed/;
 
 =head2 new
 
@@ -10,28 +11,49 @@ use Log::Log4perl;
 
 sub new {
     my $class      = shift;
-    my $shipwright = shift;
 
     my $self = {};
     bless $self, $class;
 
     if ( not Log::Log4perl->initialized ) {
-        $class->_initialize_log4perl($shipwright);
+        $class->_initialize_log4perl(@_);
     }
     return $self;
 }
 
 sub _initialize_log4perl {
-    my $class      = shift;
-    my $shipwright = shift;
+    my $class = shift;
+    my $ref   = $_[0];
 
-    my $log_level = uc $shipwright->log_level || 'ERROR';
+    my ( $log_level, $log_file );
+
+    if ( blessed $ref ) {
+
+        # so it's an object, we assuming it has log_level and log_file subs
+        $log_level = $ref->log_level;
+        $log_file  = $ref->log_file;
+    }
+    elsif ( ref $ref ) {
+
+        # it's a hashref
+        $log_level = $ref->{log_level};
+        $log_file  = $ref->{log_file};
+    }
+    else {
+
+        # not ref at all
+        my %hash = @_;
+        $log_level = $hash{log_level};
+        $log_file  = $hash{log_file};
+    }
+
+    $log_level = uc $log_level || 'ERROR';
     my %default;
 
-    if ( $shipwright->log_file ) {
+    if ($log_file) {
         %default = (
             'log4perl.rootLogger'             => "$log_level,File",
-            'log4perl.appender.File.filename' => $shipwright->log_file,
+            'log4perl.appender.File.filename' => $log_file,
             'log4perl.appender.File'        => 'Log::Log4perl::Appender::File',
             'log4perl.appender.File.stderr' => 1,
             'log4perl.appender.File.layout' =>
