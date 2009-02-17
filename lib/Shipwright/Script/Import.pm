ae4a1206c54fcc458e830c22a03b59c9ada1d149
Jesse Vincent <jesse@bestpractical.com>
2009-02-17 00:07:10 +0000

    Make import of makemaker dists use LIB and PREFIX, so we don't need to depend on a new MakeMaker

--- lib/Shipwright/Script/Import.pm
+++ lib/Shipwright/Script/Import.pm
@@ -348,7 +348,7 @@ sub _generate_build {
     elsif ( -f catfile( $source_dir, 'Makefile.PL' ) ) {
         print "detected ExtUtils::MakeMaker build system\n";
         @commands = (
-            'configure: %%PERL%% Makefile.PL INSTALL_BASE=%%INSTALL_BASE%%',
+            'configure: %%PERL%% Makefile.PL LIB=%%INSTALL_BASE%%/lib/perl5/ PREFIX=%%INSTALL_BASE%%',
             'make: %%MAKE%%',
             'test: %%MAKE%% test',
             'install: %%MAKE%% install',
