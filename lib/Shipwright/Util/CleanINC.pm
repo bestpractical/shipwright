4d1f8cfa18176939ca8570b64792ad7962ab6126
Jesse Vincent <jesse@bestpractical.com>
2009-02-18 22:51:10 +0000

    When a build script calls perl -I, don't strip it from @INC

--- lib/Shipwright/Util/CleanINC.pm
+++ lib/Shipwright/Util/CleanINC.pm
@@ -4,12 +4,48 @@ use warnings;
 use Config;
 
 sub import {
+
+    # It's expensive to do, but we need to find out what's in @INC now that
+    # should be kept (because it was specified on the commandline with -I)
+    # and what we should drop because it's baked into perl as a local lib
+    my %to_kill = map { $_ => 1} _default_inc();
+    my @inc_to_save;
+    for (@INC) {
+        next if exists $to_kill{$_};
+                push @inc_to_save, $_;
+            }
     @INC = (
+        @inc_to_save,
+        '.',
+        $ENV{PERL5LIB} ? split( ':', $ENV{PERL5LIB} ) : (),
         $Config::Config{privlibexp},
         $Config::Config{archlibexp},
-        $ENV{PERL5LIB} ? split( ':', $ENV{PERL5LIB} ) : (),
-        '.',
     );
+}
+
+
+
+{
+
+    # This code stolen from 
+    # http://cpansearch.perl.org/src/ANDYA/Test-Harness-3.15/lib/Test/Harness.pm
+    # Cache this to avoid repeatedly shelling out to Perl.
+    my @inc;
+
+    sub _default_inc {
+        return @inc if @inc;
+        local $ENV{PERL5LIB} = '';
+        local $ENV{PERLLIB} = '';
+        local $ENV{PERL5DB} = '';
+        local $ENV{PERL5OPT} = '';
+        local $ENV{PERL5ENV} = '';
+
+
+        my $perl =  $^X;
+        # Avoid using -l for the benefit of Perl 6
+        chomp( @inc = `$perl -e "print join qq[\\n], \@INC, q[]"` );
+        return @inc;
+    }
 }
 
 package inc::Shipwright::Util::CleanINC;
