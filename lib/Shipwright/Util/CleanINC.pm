33a2c3d797bad787af5fcb15abfc567c77094d0d
Jesse Vincent <jesse@bestpractical.com>
2009-02-19 02:26:45 +0000

    make sure inc gets included _after_ the blib from the built directory - makes module::build happier

--- lib/Shipwright/Util/CleanINC.pm
+++ lib/Shipwright/Util/CleanINC.pm
@@ -4,23 +4,17 @@ use warnings;
 use Config;
 
 sub import {
-
     # It's expensive to do, but we need to find out what's in @INC now that
     # should be kept (because it was specified on the commandline with -I)
     # and what we should drop because it's baked into perl as a local lib
-    my %to_kill = map { $_ => 1} _default_inc();
-    my @inc_to_save;
-    for (@INC) {
-        next if exists $to_kill{$_};
-                push @inc_to_save, $_;
-            }
-    @INC = (
-        @inc_to_save,
-        '.',
-        $ENV{PERL5LIB} ? split( ':', $ENV{PERL5LIB} ) : (),
-        $Config::Config{privlibexp},
-        $Config::Config{archlibexp},
-    );
+    
+    my %skip_lib_path = map { $_ => 1 } _default_inc();
+    delete $skip_lib_path{'.'}; # . is important
+    my @explicit_libs = grep {!/inc$/}  split( /:/,($ENV{'PERL5LIB'} ||''));
+    my @inc_libs = grep {/inc$/}  split( /:/,($ENV{'PERL5LIB'} ||''));
+    # if the libs are explicitly specified, don't pull them from @INC
+    my @new_base_inc = grep { !$skip_lib_path{$_}++ } (  @explicit_libs, @INC,@inc_libs);
+    @INC = ( @new_base_inc, $Config::Config{privlibexp}, $Config::Config{archlibexp});
 }
 
 
