efaee43c5f63a3bb55e329679c65fae3668426e0
sunnavy <sunnavy@bestpractical.com>
2010-03-11 22:42:05 +0800

    copyright year update

--- lib/Shipwright/Source/Base.pm
+++ lib/Shipwright/Source/Base.pm
@@ -712,7 +712,7 @@ sunnavy  C<< <sunnavy@bestpractical.com> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Shipwright is Copyright 2007-2009 Best Practical Solutions, LLC.
+Shipwright is Copyright 2007-2010 Best Practical Solutions, LLC.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
