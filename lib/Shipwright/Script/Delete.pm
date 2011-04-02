22b2306ca3d29abb4ead10d403f6a9c2d3d9a8bb
sunnavy <sunnavy@bestpractical.com>
2011-04-02 19:35:49 +0800

    update copyright to 2011

--- lib/Shipwright/Script/Delete.pm
+++ lib/Shipwright/Script/Delete.pm
@@ -97,7 +97,7 @@ sunnavy  C<< <sunnavy@bestpractical.com> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Shipwright is Copyright 2007-2010 Best Practical Solutions, LLC.
+Shipwright is Copyright 2007-2011 Best Practical Solutions, LLC.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
--- lib/Shipwright/Source/Directory.pm
+++ lib/Shipwright/Source/Directory.pm
@@ -85,7 +85,7 @@ sunnavy C<< <sunnavy@bestpractical.com> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright 2007-2010 Best Practical Solutions LLC.
+Copyright 2007-2011 Best Practical Solutions LLC.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
